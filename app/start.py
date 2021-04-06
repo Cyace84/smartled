9edfcb1f618088c6b35a1bf2c97f73d9ece839df
cyace84 <cyace84@gmail.com>
2021-04-06 20:04:14 +0300

    ч

--- app/start.py
+++ app/start.py
@@ -39,26 +39,7 @@ def index(request, data):
     server.send(response)
 
 
-np1 = neopixel.NeoPixel(machine.Pin(2), 50)
-np2 = neopixel.NeoPixel(machine.Pin(4), 50)
-np3 = neopixel.NeoPixel(machine.Pin(5), 50)
-np4 = neopixel.NeoPixel(machine.Pin(19), 50)
-np5 = neopixel.NeoPixel(machine.Pin(21), 50)
-np6 = neopixel.NeoPixel(machine.Pin(23), 50)
 
-np1[0] = (0,0,255)
-np2[0] = (0,220,0)
-np3[0] = (0,0,220)
-np4[0] = (50,0,0)
-np5[0] = (0,50,0)
-np6[0] = (0,0,50)
-
-np1.clear()
-np2.clear()
-np3.clear()
-np4.clear()
-np5.clear()
-np6.clear()
 
 
 server.add_route("/", index)

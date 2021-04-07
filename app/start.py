bd8c9ae0c692da3556b9a576ec05dee3efac41c3
cyace84 <cyace84@gmail.com>
2021-04-08 02:05:12 +0300

    wqe

--- app/start.py
+++ app/start.py
@@ -9,39 +9,6 @@ import uasyncio
 import machine, neopixel
 import re
 
-
-st123 = neopixel.NeoPixel(machine.Pin(4), 330)
-st134 = neopixel.NeoPixel(machine.Pin(5), 327)
-tg1 = neopixel.NeoPixel(machine.Pin(19), 149)
-tg2 = neopixel.NeoPixel(machine.Pin(21), 149)
-tg3 = neopixel.NeoPixel(machine.Pin(22), 149)
-tg4 = neopixel.NeoPixel(machine.Pin(23), 149)
-
-
-async def set_color(pin, color: tuple):
-    for i in range(pin.n):
-        pin[i] = color
-    print(1)
-    await pin.write()
-
-set_color
-
-
-
-""" start server """
-
-
-import esp
-import network
-import time
-import socket
-import sys
-import io
-import os
-import uasyncio
-import machine, neopixel
-import re
-
 from app import secrets
 
 wlan_id = secrets.WIFI_SSID
@@ -72,7 +39,7 @@ def index(request, data):
     server.send(response)
 
 
-pixels = neopixel.NeoPixel(machine.Pin(4), 330, timing = 0)
+pixels = neopixel.NeoPixel(machine.Pin(4), 330, timi)
 st134 = neopixel.NeoPixel(machine.Pin(5), 327)
 tg1 = neopixel.NeoPixel(machine.Pin(19), 149)
 tg2 = neopixel.NeoPixel(machine.Pin(21), 149)
@@ -85,37 +52,64 @@ def set_color(pin, color: tuple):
         pin[i] = color
     pin.write()
 
-def demo(np):
-    n = np.n
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
 
 
-    # bounce
-    for i in range(4 * n):
-        for j in range(n):
-            np[j] = (0, 0, 128)
-        if (i // n) % 2 == 0:
-            np[i % n] = (0, 0, 0)
-        else:
-            np[n - 1 - (i % n)] = (0, 0, 0)
-        np.write()
-        time.sleep_ms(60)
+def color_chase(color, wait):
+    for i in range(num_pixels):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.write()
+        time.sleep(0.5)
 
-    # fade in/out
-    for i in range(0, 4 * 256, 8):
-        for j in range(n):
-            if (i // 256) % 2 == 0:
-                val = i & 0xff
-            else:
-                val = 255 - (i & 0xff)
-            np[j] = (val, 0, 0)
-        np.write()
 
-    # clear
-    for i in range(n):
-        np[i] = (0, 0, 0)
-    np.write()
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            rc_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(rc_index & 255)
+        pixels.write()
 
-demo(st123)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+
+while True:
+    pixels.fill(RED)
+    pixels.show()
+    # Increase or decrease to change the speed of the solid color change.
+    time.sleep(1)
+    pixels.fill(GREEN)
+    pixels.show()
+    time.sleep(1)
+    pixels.fill(BLUE)
+    pixels.show()
+    time.sleep(1)
+
+    color_chase(RED, 0.1)  # Increase the number to slow down the color chase
+    color_chase(YELLOW, 0.1)
+    color_chase(GREEN, 0.1)
+    color_chase(CYAN, 0.1)
+    color_chase(BLUE, 0.1)
+    color_chase(PURPLE, 0.1)
+
+    rainbow_cycle(0)  # Increase the number to slow down the rainbow
 
 server.add_route("/", index)
 server

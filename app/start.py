4139df59add5c903e96600efd3ab5c5b3b72abd6
cyace84 <cyace84@gmail.com>
2021-04-08 01:35:19 +0300

    ds

--- app/start.py
+++ app/start.py
@@ -9,6 +9,39 @@ import uasyncio
 import machine, neopixel
 import re
 
+
+st123 = neopixel.NeoPixel(machine.Pin(4), 330)
+st134 = neopixel.NeoPixel(machine.Pin(5), 327)
+tg1 = neopixel.NeoPixel(machine.Pin(19), 149)
+tg2 = neopixel.NeoPixel(machine.Pin(21), 149)
+tg3 = neopixel.NeoPixel(machine.Pin(22), 149)
+tg4 = neopixel.NeoPixel(machine.Pin(23), 149)
+
+
+async def set_color(pin, color: tuple):
+    for i in range(pin.n):
+        pin[i] = color
+    print(1)
+    await pin.write()
+
+set_color
+
+
+
+""" start server """
+
+
+import esp
+import network
+import time
+import socket
+import sys
+import io
+import os
+import uasyncio
+import machine, neopixel
+import re
+
 from app import secrets
 
 wlan_id = secrets.WIFI_SSID
@@ -52,20 +85,44 @@ def set_color(pin, color: tuple):
         pin[i] = color
     pin.write()
 
-st123[329] = (0, 0, 205)
-st134[0] = (0, 205, 0)
-st123.write()
-st134.write()
-tg1[0] = (50, 0, 205)
-tg1[148] = (205, 0, 0)
-tg1.write()
+def demo(np):
+    n = np.n
 
-tg2[148] = (111, 111, 111)
-tg2.write()
-tg3[148] = (111, 111, 111)
-tg3.write()
-tg4[148] = (111, 111, 111)
-tg4.write()
+    # cycle
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 0)
+        np[i % n] = (255, 255, 255)
+        np.write()
+        time.sleep_ms(25)
+
+    # bounce
+    for i in range(4 * n):
+        for j in range(n):
+            np[j] = (0, 0, 128)
+        if (i // n) % 2 == 0:
+            np[i % n] = (0, 0, 0)
+        else:
+            np[n - 1 - (i % n)] = (0, 0, 0)
+        np.write()
+        time.sleep_ms(60)
+
+    # fade in/out
+    for i in range(0, 4 * 256, 8):
+        for j in range(n):
+            if (i // 256) % 2 == 0:
+                val = i & 0xff
+            else:
+                val = 255 - (i & 0xff)
+            np[j] = (val, 0, 0)
+        np.write()
+
+    # clear
+    for i in range(n):
+        np[i] = (0, 0, 0)
+    np.write()
+
+demo(st123)
 
 server.add_route("/", index)
 server

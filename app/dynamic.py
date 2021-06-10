691de58c8de5f07b66be269f8a2a996c9bf30898
cyace84 <cyace84@gmail.com>
2021-06-11 02:30:48 +0300

    gh

--- app/dynamic.py
+++ app/dynamic.py
@@ -1,13 +1,44 @@
 import time
 
-import machine, neopixel
+import machine
 import _thread as thread
-roof1 = neopixel.NeoPixel(machine.Pin(4), 330)
-roof2 = neopixel.NeoPixel(machine.Pin(5), 330)
-tg1 = neopixel.NeoPixel(machine.Pin(19), 149)
-tg2 = neopixel.NeoPixel(machine.Pin(21), 149)
-tg3 = neopixel.NeoPixel(machine.Pin(22), 149)
-tg4 = neopixel.NeoPixel(machine.Pin(23), 149)
+from esp import neopixel_write
+import uasyncio
+
+class NeoPixel:
+    ORDER = (1, 0, 2, 3)
+    __slots__ = ("pin", "n", "bpp", "buf", "timing")
+
+    def __init__(self, pin, n, bpp=3, timing=1):
+        self.pin = pin
+        self.n = n
+        self.bpp = bpp
+        self.buf = bytearray(n * bpp)
+        self.pin.init(pin.OUT)
+        self.timing = timing
+
+    def __setitem__(self, index, val):
+        offset = index * self.bpp
+        for i in range(self.bpp):
+            self.buf[offset + self.ORDER[i]] = val[i]
+
+    def __getitem__(self, index):
+        offset = index * self.bpp
+        return tuple(self.buf[offset + self.ORDER[i]] for i in range(self.bpp))
+
+    def fill(self, color):
+        for i in range(self.n):
+            self[i] = color
+
+    def write(self):
+        neopixel_write(self.pin, self.buf, self.timing)
+
+roof1 = NeoPixel(machine.Pin(4), 330)
+roof2 = NeoPixel(machine.Pin(5), 330)
+tg1 = NeoPixel(machine.Pin(19), 149)
+tg2 = NeoPixel(machine.Pin(21), 149)
+tg3 = NeoPixel(machine.Pin(22), 149)
+tg4 = NeoPixel(machine.Pin(23), 149)
 
 modes = {
     "rainbow": "on"
@@ -45,20 +76,38 @@ def brightness_control(color, brightness=1):
 pin2 = machine.Pin(2, machine.Pin.OUT)
 
 
+def set_color(j, led, n1, n2):
+    for i in range(n1, n2):
+        rc_index = (i * 256 // 328) + j
+        led[i] = brightness_control(wheel(rc_index & 255))
+
+
+
 def rainbow_cycle(slow=0):
     j = 0
+    li = []
+
     while modes["rainbow"] == "on":
         j += 1
-        for i in range(30):
+        ss = 0
+        for i in range(330):
+            ss+= 1
             rc_index = (i * 256 // 328) + j
-            roof1[i] = brightness_control(wheel(rc_index & 255), brightness=0.5)
-        time.sleep(slow)
+            roof1[i] = wheel(rc_index & 255)
+            if ss > 29:
+                roof1.write()
+                ss = 0
+
+
         roof1.write()
-
+        li.append(time.time())
+        if len(li) > 20:
+            modes["rainbow"] = "off"
+            print(li[0], li[-1:])
     return
-
 
 
 def create_task():
     thread.start_new_thread(rainbow_cycle, ())
 
+create_task()
